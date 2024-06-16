08a9ddbea906a38becacd3868e7a1dc778f3b3df
shibu <akihirokaneda@outlook.com>
2024-06-16 23:04:07 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+document.getElementById('generateButton').addEventListener('click', () => {
+  const inputText = document.getElementById('inputText').value;
+  const outputText = document.getElementById('outputText');
+  outputText.textContent = generateRandomMojibake(inputText);
+  document.getElementById('errorMessage').style.display = 'none'; // エラーメッセージを非表示にする
+});
+
+document.getElementById('copyButton').addEventListener('click', () => {
+  const outputText = document.getElementById('outputText').textContent;
+  if (outputText === '') {
+    const errorMessage = document.getElementById('errorMessage');
+    errorMessage.style.display = 'block';
+    setTimeout(() => {
+      errorMessage.style.display = 'none';
+    }, 2000); // 2秒後にエラーメッセージを非表示にする
+  } else {
+    navigator.clipboard.writeText(outputText).then(() => {
+      const copyMessage = document.getElementById('copyMessage');
+      copyMessage.style.display = 'block';
+      setTimeout(() => {
+        copyMessage.style.display = 'none';
+      }, 2000); // 2秒後にメッセージを非表示にする
+    });
+  }
+});
+
+function generateRandomMojibake(text) {
+  const mojibakeChars = '繧ゅ§縺ｰ縺代ｒ繧医�縲�';
+  let result = '';
+  for (let i = 0; i < text.length; i++) {
+    // ランダムに文字を選択
+    result += mojibakeChars.charAt(
+      Math.floor(Math.random() * mojibakeChars.length)
+    );
+  }
+  return result;
+}
