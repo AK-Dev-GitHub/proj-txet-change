5474e579bda8279c166addb5ac8d3258d32959d1
shibu <akihirokaneda@outlook.com>
2024-06-18 11:53:20 +0900

    Fix : ランキングページ追加/ランキングアイコン追加/レスポンシブ対応追加/文字化け解除ボタン追加/解除メッセージ追加

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ document.getElementById('inputText').addEventListener('input', () => {
   const inputText = document.getElementById('inputText').value;
   const generateButton = document.getElementById('generateButton');
   const resetButton = document.getElementById('resetButton');
+  const restoreButton = document.getElementById('restoreButton');
   generateButton.disabled = inputText.trim() === '';
   resetButton.disabled = inputText.trim() === '';
+  restoreButton.disabled = inputText.trim() === '';
 });
 
 document.getElementById('generateButton').addEventListener('click', () => {
   const inputText = document.getElementById('inputText').value;
   const outputText = document.getElementById('outputText');
   outputText.textContent = generateRandomMojibake(inputText);
-  document.getElementById('copyButton').disabled = false; // コピーボタンを活性化
+  document.getElementById('copyButton').disabled = false;
+  document.getElementById('restoreButton').disabled = false; // 文字化け解除ボタンを活性化
 });
 
 document.getElementById('copyButton').addEventListener('click', () => {
@@ -24,13 +27,13 @@ document.getElementById('copyButton').addEventListener('click', () => {
   });
 });
 
-// リセットボタンを押下した場合の処理
 document.getElementById('resetButton').addEventListener('click', () => {
   document.getElementById('inputText').value = '';
   document.getElementById('outputText').textContent = '';
   document.getElementById('generateButton').disabled = true;
   document.getElementById('copyButton').disabled = true;
   document.getElementById('resetButton').disabled = true;
+  document.getElementById('restoreButton').disabled = true;
   const resetMessage = document.getElementById('resetMessage');
   resetMessage.style.display = 'block';
   setTimeout(() => {
@@ -38,9 +41,19 @@ document.getElementById('resetButton').addEventListener('click', () => {
   }, 2000); // 2秒後にメッセージを非表示にする
 });
 
-// ランダムな文字列を生成する関数
+document.getElementById('restoreButton').addEventListener('click', () => {
+  const inputText = document.getElementById('inputText').value;
+  document.getElementById('outputText').textContent = inputText;
+  const restoreMessage = document.getElementById('restoreMessage');
+  restoreMessage.style.display = 'block';
+  setTimeout(() => {
+    restoreMessage.style.display = 'none';
+  }, 2000); // 2秒後にメッセージを非表示にする
+});
+
 function generateRandomMojibake(text) {
-  const mojibakeChars = '！繧ゅ§縺ｰ縺代ｒ繧医�縲�殺肉';
+  const mojibakeChars =
+    '！繧ゅ§縺ｰ縺代ｒ繧医�縲�殺繝帙繝代谺縺ｯ雋ｷ縺縺ｦ縺上繝斐?驥鷹ｫｪ鄒主ｳ縺瑚縺ｫ縺輔繧句虚逕ｻ肉ォ逅';
   let result = '';
   for (let i = 0; i < text.length; i++) {
     // ランダムに文字を選択
