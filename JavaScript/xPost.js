41f9e38e1cf4b090eb0b0f1ea6b485fcc29dad52
shibu <akihirokaneda@outlook.com>
2024-10-28 08:34:10 +0900

    Fix : ハンバーガーメニュー修正・使い方ページのStyle修正

--- JavaScript/xPost.js
+++ JavaScript/xPost.js
@@ -1,5 +1,18 @@
 // Xにポスト
-document.getElementById('twitterLink').addEventListener('click', (event) => {
+document.getElementById('XLinkDeskTop').addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent the default anchor behavior
+  const text = [
+    '文字化けジェネレータで生成しました！: ' +
+      document.getElementById('outputText').textContent,
+    'https://minna-no-mojibake.com/',
+  ].join('\n');
+  const url =
+    'https://twitter.com/intent/tweet?text=' + encodeURIComponent(text);
+  window.open(url, '_blank');
+});
+
+// Xにポスト（ハンバーガーメニュー）
+document.getElementById('XLinkMobile').addEventListener('click', (event) => {
   event.preventDefault(); // Prevent the default anchor behavior
   const text = [
     '文字化けジェネレータで生成しました！: ' +
